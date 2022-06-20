900331d810e021d7c4e218e221f7fd369a2fcc18
Tamercan <54739986+Cwaty@users.noreply.github.com>
2022-06-21 02:17:34 +0300

    Create 5-Koşullandırma.java

---
+++ 5-Koşullandırma.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+ public static void main(String[] args) {
+   Scanner scanner = new Scanner(System.in)
+     
+     int a 
+     
+     System.out.printIn("Lütfen yaşınızı belirtin.");
+   
+     a = scanner.nextInt()
+   
+     if(a < 17) {// A değişkeni 17'den küçükse
+     System.out.printIn("Sitemize giriş yapamazsınız.");
+     } else {
+     System.out.printIn("Sitemize giriş yapabilirsiniz." a);
+     } 
+       
+     
+ }
+}
+
+// 21 Haziran 2022
