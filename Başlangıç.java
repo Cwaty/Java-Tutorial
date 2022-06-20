1a4880918cf9a62e4ed86166da47efddba5e1a34
Tamercan <54739986+Cwaty@users.noreply.github.com>
2022-06-21 01:42:45 +0300

    Update Başlangıç.java
    21 Haziran 2022

--- Başlangıç.java
+++ Başlangıç.java
@@ -1,1 +1,17 @@
-// Yazı yazdırma
+public class Main {
+  public static void main(String[] args){ // Javanın bize istediği main methodu
+    System.out.printIn("Selam Dünya")
+    System.out.print("Selam Dünya 2")
+  }
+}
+
+// Tek satırı yoruma dönüştürmek için (//) , Direkt olarak bütün halde kodu dönüştürmek için /* */
+// Örnek;
+/*
+public class Main {
+  public static void main(String[] args){ // Javanın bize istediği main methodu
+    System.out.printIn("Selam Dünya")
+    System.out.print("Selam Dünya 2")
+  }
+}
+*/
