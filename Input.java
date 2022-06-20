57d08972ad38fffe984b08e5bb9fca4fcbc202ab
Tamercan <54739986+Cwaty@users.noreply.github.com>
2022-06-21 02:05:56 +0300

    Create Input.java

---
+++ Input.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+ public static void main(String[] args) {
+   Scanner scanner = new Scanner(System.in)
+     
+     int a,b // İki tane input alınacak.
+     
+     System.out.printIn("Lütfen sayılarınızı belirtin.");
+   
+     a = scanner.nextInt()
+     b = scanner.nextInt()
+       
+     System.out.printIn("Yazılan sayılar" + (a,b))
+ }
+}
+
+// 21 Haziran 2022
