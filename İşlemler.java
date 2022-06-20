1def8711d0a14c34d2c3c039a44e94dbc480ed91
Tamercan <54739986+Cwaty@users.noreply.github.com>
2022-06-21 01:55:19 +0300

    Create İşlemler.java

---
+++ İşlemler.java
@@ -0,0 +1,18 @@
+int a = 99
+int b = 1
+  
+System.out.printIn(a + b); // Toplama.
+System.out.printIn(a - b); // Çıkartma.
+System.out.printIn(a * b); // Çarpma.
+System.out.printIn(a / b); // Bölme.
+System.out.printIn(a % b); // Bölümünden kalan.
+System.out.printIn(a / (double)b); // Ondalıklı alma double.
+
+// Arttırma
+
+int b = 5
+b = b + 1
+b += 1
+b++
+  
+// 21 Haziran 2022
