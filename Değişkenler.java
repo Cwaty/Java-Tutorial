d0fee327fdb166ae9d6ecc5ad5c63f4afe0b7293
Tamercan <54739986+Cwaty@users.noreply.github.com>
2022-06-21 01:49:27 +0300

    Create Değişkenler.java

---
+++ Değişkenler.java
@@ -0,0 +1,9 @@
+int a = 32; // Belirli bir şeyi tanımlar.
+double b = 3.14; // Ondalıklı sayıları tanımlar.
+float c = 3.14f; // Daha az yer kaplayan ondalıklı veriler.
+char d = "a"; // Tek bir karakteri taşır.
+String test = "Selamlar";
+boolean e = false; // False veya True dışında başka değişken kullanılmaz.
+
+System.out.printIn("A verisi" + a); // Çıkan: A verisi 32.
+  
