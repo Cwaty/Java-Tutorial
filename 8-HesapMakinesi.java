f52dc7a078b2af9fb705d51c95d880b8edde2851
Tamercan <54739986+Cwaty@users.noreply.github.com>
2022-06-21 12:45:37 +0300

    Create 8-HesapMakinesi.java

---
+++ 8-HesapMakinesi.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Selam.");
+
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("İşlem uygulamak istediğiniz birinci sayıyı giriniz.");
+        int sayi1 = scanner.nextInt();
+        System.out.println("İşlem uygulamak istediğiniz ikinci sayıyı giriniz.");
+        int sayi2 = scanner.nextInt();
+
+        System.out.println("İşleminizi seçiniz (+ , - , * , /)");
+        char islem = scanner.next().charAt(0);
+
+        int sonuc;
+
+        switch (islem) {
+            case "+":
+                sonuc = sayi1 + sayi2;
+                break;
+            case "-":
+                sonuc = sayi1 - sayi2;
+                break;
+            case "*":
+                sonuc = sayi1 * sayi2;
+                break;
+            case "/":
+                sonuc = sayi1 / sayi2;
+                break;
+            default:
+            System.out.println("Geçersiz işlem.");
+                break;
+        }
+        syste.out.println("İşleminizin sonucu: " + sonuc);
+    }
+
+}
