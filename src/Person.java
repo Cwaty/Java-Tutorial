38584887a6246bec1aabd91612c093c24c22c5de
Tamercan <54739986+Cwaty@users.noreply.github.com>
2022-06-21 23:47:33 +0300

    Create Person.java

---
+++ src/Person.java
@@ -0,0 +1,11 @@
+public class Person {
+    private String name; // at
+
+    public void setName(String name){
+        this.name = name;
+        System.out.println("Isim basariyla degistirildi.");
+    }
+    public String getName() {
+        return name;
+    }
+}
